392cf04d121184d58c830c06994212874ff14a0f
Joshua Barrientos <56335048+crimlast@users.noreply.github.com>
2022-11-06 19:33:12 -0800

    addition of some art assets

---
+++ Assets/Scripts/ObjectInteraction/Breakable.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Breakable : MonoBehaviour
+{
+    public int health;
+    private GameObject obj;
+    // Start is called before the first frame update
+    void Start()
+    {
+        health = 5;
+        obj = this.gameObject;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        //TODO : condition to damage health
+        if(health <= 0){
+            Destroy(obj); //destroy the current object this script is attached to;
+        }
+    }
+
+}
