5644dbc67e958531e62c607c982f87d3464e23b9
Joshua Barrientos <56335048+crimlast@users.noreply.github.com>
2022-11-06 20:28:58 -0800

    update room Object scripts

---
+++ Assets/Scripts/ObjectInteraction/Interactable.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Interactable : MonoBehaviour
+{
+    private bool doInteract;
+    private GameObject obj;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        doInteract = false;        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        //TODO: condition to make doInteract 
+        if(doInteract){
+            //destroy the current object this script is attached to;
+            Destroy(obj); 
+            //condition to change something else;
+        }
+        
+    }
+}
---
+++ Assets/Scripts/ObjectInteraction/interactHealthPlus.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class interactHealthPlus : MonoBehaviour
+{
+    private bool doInteract;
+    private GameObject obj;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        doInteract = false;
+        obj = this.gameObject;
+        //health = get health from health script        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        //TODO: condition to make doInteract 
+        if(doInteract){
+            //destroy the current object this script is attached to;
+            Destroy(obj); 
+            //Increase health of player
+        }       
+    }
+}
