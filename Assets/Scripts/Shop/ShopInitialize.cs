873c7c0dd1ba8f3aa069b1616bd4212cb027375e
Joshua Barrientos <56335048+crimlast@users.noreply.github.com>
2022-11-20 17:30:46 -0800

    shop changes and other small details

---
+++ Assets/Scripts/Shop/ShopInitialize.cs
@@ -0,0 +1,44 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ShopInitialize : MonoBehaviour
+{
+    [SerializeField] GameObject shopUI;
+    public bool ableToRunShop;
+    // Start is called before the first frame update
+    void Start()
+    {
+        ableToRunShop = false;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(ableToRunShop && (Input.GetKeyDown(KeyCode.E))){
+            InitializeUIShop();
+        }
+    }
+    
+    void OnTriggerEnter2D(Collider2D col){
+        if(col.gameObject.tag == "Player"){
+            ableToRunShop = true;
+        }
+    }
+
+    void OnTriggerExit2D(Collider2D col){
+        if(col.gameObject.tag == "Player"){
+            ableToRunShop = false;
+        }
+    }
+    
+    void InitializeUIShop(){
+        Time.timeScale = 0;
+        shopUI.SetActive(true);
+    }
+
+    public void ExitShop(){
+        Time.timeScale = 1;
+        shopUI.SetActive(false);
+    }
+}
