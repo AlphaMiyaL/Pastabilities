db510327d95502a9a34c3302e067d879937d0447
dev0nati0n <112506177+dev0nati0n@users.noreply.github.com>
2022-11-23 01:57:58 -0800

    Revamped Sound Manager
    Redid the sound manager code. Now it can separately adjust the volume of the master, music, and SFX separately. Volume sliders will now save their positions even after exiting the scene

---
+++ Assets/Scripts/Sound/VolumeSettings.cs
@@ -0,0 +1,53 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Audio;
+using UnityEngine.UI;
+
+public class VolumeSettings : MonoBehaviour
+{
+    [SerializeField] AudioMixer mixer;
+    [SerializeField] Slider masterSlider;
+    [SerializeField] Slider musicSlider;
+    [SerializeField] Slider sfxSlider;
+
+    public const string MIXER_MASTER = "MasterVolume";
+    public const string MIXER_MUSIC = "MusicVolume";
+    public const string MIXER_SFX = "SFXVolume";
+
+    void Awake()
+    {
+        masterSlider.onValueChanged.AddListener(SetMasterVolume);
+        musicSlider.onValueChanged.AddListener(SetMusicVolume);
+        sfxSlider.onValueChanged.AddListener(SetSFXVolume);
+    }
+
+    void Start()
+    {
+        masterSlider.value = PlayerPrefs.GetFloat(AudioManager.MASTER_KEY, 1f);
+        musicSlider.value = PlayerPrefs.GetFloat(AudioManager.MUSIC_KEY, 1f);
+        sfxSlider.value = PlayerPrefs.GetFloat(AudioManager.SFX_KEY, 1f);
+    }
+
+    void OnDisable()
+    {
+        PlayerPrefs.SetFloat(AudioManager.MASTER_KEY, masterSlider.value);
+        PlayerPrefs.SetFloat(AudioManager.MUSIC_KEY, musicSlider.value);
+        PlayerPrefs.SetFloat(AudioManager.SFX_KEY, sfxSlider.value);
+    }
+
+    void SetMasterVolume(float volume)
+    {
+        mixer.SetFloat(MIXER_MASTER, Mathf.Log10(volume) * 20);
+    }
+
+    void SetMusicVolume(float volume)
+    {
+        mixer.SetFloat(MIXER_MUSIC, Mathf.Log10(volume) * 20);
+    }
+
+    void SetSFXVolume(float volume)
+    {
+        mixer.SetFloat(MIXER_SFX, Mathf.Log10(volume) * 20);
+    }
+}
