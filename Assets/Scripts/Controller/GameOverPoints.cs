d8ea18c71b5d0654c0e00e09025090c138c1be0b
Jihyun Lee <jlee310@go.pasadena.edu>
2022-11-30 05:38:44 -0800

    stuff

---
+++ Assets/Scripts/Controller/GameOverPoints.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class GameOverPoints : MonoBehaviour
+{
+    public Text pointSystem;
+    //public int points;
+
+    void Awake()
+    {
+        PointController pointController = GameObject.FindGameObjectWithTag("Points").GetComponent<PointController>();
+        pointSystem.text = "" + pointController.points;
+    }
+
+}
